ca6591577c13cf91ac9ddc73416fcd6091c6fd48
Azizbek Alisherov <azizalisherov0826@gmail.com>
2025-05-14 18:49:25 +0500

    CHANGED

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -34,7 +34,7 @@ const Blogs = () => {
     <div id="blogs">
       <div className="container mt-[30px] m-auto">
         <div
-          className="top container mt-[100px] w-full h-[237px] rounded-2xl p-[70px] "
+          className="top container mt-[200px] w-full h-[237px] rounded-2xl p-[70px] "
           style={{ backgroundImage: `url(${bgImage})` }}
         >
           <div className="top-details flex flex-col lg:flex-row justify-between gap-4">
