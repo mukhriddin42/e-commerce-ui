a004ed4652008f763cbec16755599e13f41a4689
asadbek haydarov <asadbekcoder1@gmail.com>
2025-05-14 16:41:55 +0500

    fix: fixed router dom bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import LayoutAbout from "./components/LayoutAbout/LayoutAbout";
+import About from "./pages/About";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="/" element={<h1>home</h1>}></Route>
-            <Route path="/about" element={<h1>About</h1>}></Route>
-            <Route path="/myaccount" element={<h1>My Account</h1>}></Route>
-            <Route path="/wishlist" element={<h1>Wishlist</h1>}></Route>
+          <Route path="/" element={<LayoutAbout />}>
+            <Route path="/" element={<h1>Home</h1>}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/home" element={<About />}></Route>
+          </Route>
+          {/* <Route path="/" element={<Layout />}>
+            <Route index element={<h1>home</h1>}></Route>
+            <Route path="aboutus" element={<h1>About Us</h1>}></Route>
+            <Route path="myaccount" element={<h1>My Account</h1>}></Route>
+            <Route path="wishlist" element={<h1>Wishlist</h1>}></Route>
             <Route
-              path="/ordertracking"
+              path="ordertracking"
               element={<h1>Order Tracking</h1>}
             ></Route>
-          </Route>
+          </Route> */}{" "}
+          {/* teginilmasin  */}
         </Routes>
       </BrowserRouter>
     </>
