e24e8fb8076a9a7182a9692783c3867ff99341bb
haydarov_dev <122049418+HaydarovDev@users.noreply.github.com>
2025-05-21 18:47:25 +0500

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
-import TrendingProducts from "../components/TrendingProducts/TrendingProducts";
-import ShopCategory from "../components/Shop/ShopCategory";
-import Category from "../components/Category/Category";
-import StayHome from "../components/StayHome/StayHome";
-import Nav from "../components/Nav/Nav";
-import Menu from "../components/Menu/Menu";
+// import TrendingProducts from "../components/TrendingProducts/TrendingProducts";
+// import ShopCategory from "../components/Shop/ShopCategory";
+// import Category from "../components/Category/Category";
+// import StayHome from "../components/StayHome/StayHome";
+// import Nav from "../components/Nav/Nav";
+// import Menu from "../components/Menu/Menu";
+import Tabpanel from "../components/Home2/Tabpanel";
+import Deals_of_the_day from "../components/Home2/Deals_of_the_day";
+import Product_card from "../components/Home2/Product_card";
 
 const Home = () => {
   return (
@@ -15,6 +18,9 @@ const Home = () => {
         <div className="w-full max-w-screen-xl px-4">
           <div className="flex flex-col w-full gap-5">
             {/* <ShopCategory /> */}
+            <Tabpanel />
+            <Product_card />
+            <Deals_of_the_day />
           </div>
         </div>
       </div>
