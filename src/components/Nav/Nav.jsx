b88021b2ce136ccbf4e396f68bb6a78e5f0460e5
haydarov_dev <122049418+HaydarovDev@users.noreply.github.com>
2025-05-28 18:26:18 +0500

    update

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -11,7 +11,7 @@ import { RxHamburgerMenu } from "react-icons/rx";
 const Nav = ({ onToggleMenu }) => {
   return (
     <div className="fixed top-0 bg-white w-full z-1000">
-      <div className="flex w-full shadow-sm xl:hidden 2xl:hidden py-5 justify-center">
+      {/* <div className="flex w-full shadow-sm xl:hidden 2xl:hidden py-5 justify-center">
         <div className="flex items-center justify-between w-[90%]">
           <button className="border p-1 rounded">
             <RxHamburgerMenu className="text-2xl" onClick={onToggleMenu} />
@@ -32,14 +32,18 @@ const Nav = ({ onToggleMenu }) => {
             </li>
           </ul>
         </div>
-      </div>
-      <nav className="hidden sm:hidden md:hidden xl:flex 2xl:flex items-center justify-between px-20 py-5 border-b border-[#ECECEC]">
+      </div> */}
+      <nav className="flex  items-center justify-between px-20 gap-3 py-5 border-b border-[#ECECEC]">
+        <RxHamburgerMenu
+          className="text-2xl hidden md-block"
+          onClick={onToggleMenu}
+        />
         <Link to="/">
           <img className="max-w-[150px]" src={logo} alt="" />
         </Link>
         <label
           htmlFor=""
-          className="flex border-[2px] border-[#BCE3C9] rounded-[2px] p-2 gap-2 items-center"
+          className="hidden border-[2px] border-[#BCE3C9] rounded-[2px] p-2 gap-2 items-center sm:hidden md:hidden lg:flex xl:flex"
         >
           <select name="" id="" className="outline-0 px-2">
             <option value="">All Categories</option>
@@ -50,13 +54,13 @@ const Nav = ({ onToggleMenu }) => {
           <input
             type="text"
             placeholder="Search for items..."
-            className="border-0 outline-0 w-[400px]"
+            className="border-0 outline-0 w-[400px] md:w-[200px] xl:w-[400px]"
           />
           <CiSearch className="text-[20px]" />
         </label>
         <label
           htmlFor=""
-          className="border border-[#ECECEC] p-2 flex items-center rounded"
+          className="border border-[#ECECEC] p-2 hidden items-center rounded sm:hidden md:flex xl:flex"
         >
           <CiLocationOn />
           <select name="" id="" className="outline-0 text-[#3BB77E]">
@@ -66,7 +70,7 @@ const Nav = ({ onToggleMenu }) => {
           </select>
         </label>
         <ul className="flex gap-4">
-          <li className="cursor-pointer p-2">
+          <li className="cursor-pointer p-2 hidden xl:block">
             <Link
               to="/wishlist"
               className="flex items-center justify-center gap-2"
@@ -75,10 +79,10 @@ const Nav = ({ onToggleMenu }) => {
               Wishlist
             </Link>
           </li>
-          <li className="flex items-center gap-2 cursor-pointer p-2">
+          <li className="flex items-center gap-2 cursor-pointer p-2 ">
             <Link to="/" className="flex items-center justify-center gap-2">
               <FiShoppingCart className="text-2xl" />
-              Cart
+              <span className="hidden xl:block">Cart</span>
             </Link>
           </li>
           <li className="flex items-center gap-2 cursor-pointer p-2">
@@ -87,7 +91,7 @@ const Nav = ({ onToggleMenu }) => {
               className="flex items-center justify-center gap-2"
             >
               <FaRegUser className="text-2xl" />
-              Account
+              <span className="hidden xl:block">Account</span>
             </Link>
           </li>
         </ul>
