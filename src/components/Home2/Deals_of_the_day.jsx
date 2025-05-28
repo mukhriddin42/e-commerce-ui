9d0a3b200b085cad71210d18ad592e6f874484e7
javohirjon009 <javohirakramov231@gmail.com>
2025-05-28 14:49:30 +0500

    api bor

--- src/components/Home2/Deals_of_the_day.jsx
+++ src/components/Home2/Deals_of_the_day.jsx
@@ -1,46 +1,17 @@
 "use client";
+import { useEffect, useState } from "react";
 import { Star, ShoppingCart } from "lucide-react";
 import { FaAngleRight } from "react-icons/fa6";
 
 export default function ProductCards() {
-  const products = [
-    {
-      id: "1",
-      name: "Seeds of Change Organic Quinoa, Brown, & Red Rice",
-      image: "src/assets/Home/banner-5.png.png",
-      rating: 4.0,
-      vendor: "NestFood",
-      price: 32.85,
-      originalPrice: 33.8,
-    },
-    {
-      id: "2",
-      name: "Perdue Simply Smart Organics Gluten Free",
-      image: "src/assets/Home/banner-6.png.png",
-      rating: 4.0,
-      vendor: "Old El Paso",
-      price: 24.85,
-      originalPrice: 26.8,
-    },
-    {
-      id: "3",
-      name: "Signature Wood-Fired Mushroom and Caramelized",
-      image: "src/assets/Home/banner-6.png.png",
-      rating: 3.0,
-      vendor: "Progresso",
-      price: 12.85,
-      originalPrice: 13.8,
-    },
-    {
-      id: "4",
-      name: "Simply Lemonade with Raspberry Juice",
-      image: "src/assets/Home/banner-6.png.png",
-      rating: 3.0,
-      vendor: "Yoplait",
-      price: 15.85,
-      originalPrice: 16.8,
-    },
-  ];
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products?limit=4")
+      .then((res) => res.json())
+      .then((data) => setProducts(data.products))
+      .catch((error) => console.error("Error fetching products:", error));
+  }, []);
 
   return (
     <div className="py-7 container m-auto">
@@ -63,13 +34,13 @@ export default function ProductCards() {
               className="flex flex-col items-center relative pb-16"
             >
               <img
-                src={product.image}
-                alt={product.name}
+                src={product.thumbnail}
+                alt={product.title}
                 className="w-full max-w-[340px] rounded-xl shadow-md"
               />
               <div className="w-[90%] max-w-[320px] h-[165px] rounded-2xl shadow-xl absolute bg-white -bottom-10 flex flex-col gap-1 justify-center px-4">
                 <h3 className="font-medium text-gray-800 text-sm line-clamp-2">
-                  {product.name}
+                  {product.title}
                 </h3>
 
                 <div className="flex mb-1">
@@ -80,7 +51,7 @@ export default function ProductCards() {
                 </div>
 
                 <div className="text-sm text-gray-500 mb-2">
-                  By <span className="text-[#3BB77E]">{product.vendor}</span>
+                  By <span className="text-[#3BB77E]">{product.brand}</span>
                 </div>
 
                 <div className="flex items-center justify-between w-full">
@@ -89,7 +60,11 @@ export default function ProductCards() {
                       ${product.price.toFixed(2)}
                     </span>
                     <span className="text-gray-400 text-sm line-through ml-2">
-                      ${product.originalPrice.toFixed(2)}
+                      $
+                      {(
+                        product.price *
+                        (1 + product.discountPercentage / 100)
+                      ).toFixed(2)}
                     </span>
                   </div>
 
